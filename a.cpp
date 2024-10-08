2c486d0ed8318b459af568d5be2e44d58d46a33d
lcr <lcr>
2024-10-08 22:34:26 +0800

    信息检索第一次作业

--- a.cpp
+++ a.cpp
@@ -3,52 +3,44 @@
 const int N = 5e1 + 10;
 using LL = long long;
 
-void se(int x)
-{
-    std::string c = std::to_string(x) + " : ";
-    std::vector<int> v;
-    while (x)
-    {
-        v.push_back(x % 128);
-        x /= 128;
-    }
-    for (int i = v.size() - 1; i >= 0; i--)
-    {
-        c += (i ? "0" : "1") + std::bitset<7>(v[i]).to_string() + " \\; ";
-    }
-    std::cout << c << "\\\\\n";
-}
+std::map<std::string, int> ma[N];
+std::vector<std::string> v;
 
-void ve(int x)
-{
-    int u = 0, t = x;
-    std::string c, a, b;
-    while (t)
-        a += (char)('0' + (t & 1)), t >>= 1, u++;
-    std::reverse(a.begin(), a.end());
-    b += std::string(u - 1, '1') + '0';
-    c = b + a.substr(1);
-    std::cout << x << " : " << c << "\\\\\n";
-}
-int a[N];
 int main()
 {
     int n;
     std::cin >> n;
-    for (int i = 1; i <= n; i++)
-        std::cin >> a[i];
-    for (int i = 1; i <= n; i++)
-        se(a[i]);
-    for (int i = 1; i <= n; i++)
-        ve(a[i]);
-    std::cout << "\n";
-    for (int i = 1; i <= n; i++)
-        se(a[i] - a[i - 1]);
-    for (int i = 1; i <= n; i++)
-        ve(a[i] - a[i - 1]);
+    std::string ans;
+    while (n--)
+    {
+
+        int x;
+        char c[100];
+        scanf("%d %c %s", &x, c, c + 1);
+        std::string a(c + 1);
+        std::cout << x << " " << a << "\n";
+
+        ans += std::to_string(x) + "\\;:\\;";
+        // std::cout << "|||" << ans << "<<<<";
+        for (int i = 0; i < a.size(); i += 8)
+        {
+            ans += a.substr(i, 8) + "\\;";
+        }
+
+        ans += '\n';
+    }
+    std::cout << ans;
     return 0;
 }
 /*
-4
-777 17743 294068 31251336
+9
+1 : 10000
+777 : 1111111110100001001\\
+17743 : 11111111111111000010101001111\\
+294068 : 1111111111111111110000111110010110100\\
+31251336 : 1111111111111111111111110110111001101101110001000\\
+777 : 1111111110100001001\\
+16966 : 11111111111111000001001000110\\
+276325 : 1111111111111111110000011011101100101\\
+30957268 : 1111111111111111111111110110110000101111011010100\\
 */
